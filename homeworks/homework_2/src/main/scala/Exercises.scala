db2bcb7cf2e0e142b1c9cce5e17b6780dad6ada7
Denis Matafonov <dmatafonov2000@mail.ru>
2020-10-22 00:15:08 +0500

    Homework_2-Матафонов Денис

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -1,32 +1,48 @@
+import scala.collection.immutable.ListMap
+
 object Exercises {
 
-    /*ПРИМЕР*/
-    /*Реализовать функцию, которая возвращает все целые числа в заданном диапазоне (от iForm до iTo), которые делятся
+  /*ПРИМЕР*/
+  /*Реализовать функцию, которая возвращает все целые числа в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 7.*/
-    /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def divBy3Or7(iFrom: Int, iTo: Int): Seq[Int] = {
-        for {i <- iFrom to iTo
-             if i % 3 == 0 || i % 7 == 0
-        } yield i
-    }
+  /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
+  def divBy3Or7(iFrom: Int, iTo: Int): Seq[Int] = {
+    for {i <- iFrom to iTo
+         if i % 3 == 0 || i % 7 == 0
+         } yield i
+  }
 
 
-
-    /*ЗАДАНИЕ I*/
-    /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
+  /*ЗАДАНИЕ I*/
+  /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
-    /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+  /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
+  def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+    divBy3Or5(iFrom, iTo).sum
+  }
+
+  def divBy3Or5(iFrom: Int, iTo: Int): Seq[Int] = {
+    for {i <- iFrom to iTo
+         if i % 3 == 0 || i % 5 == 0
+         } yield i
+  }
 
 
-
-    /*ЗАДАНИЕ II*/
-    /*Реализовать функцию, которая вычисляет все различные простые множители целого числа отличные от 1.
+  /*ЗАДАНИЕ II*/
+  /*Реализовать функцию, которая вычисляет все различные простые множители целого числа отличные от 1.
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
-    /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+  /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
+  def primeFactor(number: Int): Seq[Int] = {
+    val res = passage(number).distinct
+    res
+  }
 
+  def passage(curVal: Int, factors: List[Int] = List()): List[Int] = {
+    for(i <- 2 to curVal if curVal % i == 0)
+      return passage(curVal / i, factors :+ i)
+    factors
+  }
 
 
     /*ЗАДАНИЕ III*/
@@ -40,16 +56,18 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, function: (Vector2D, Vector2D) => Double , rightVec0: Vector2D, rightVec1: Vector2D): Double =
+        function(leftVec0, leftVec1) + function(rightVec0, rightVec1)
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
+    def round2(i: Double): Double ={ //для удобства тестирования
+        Math.round(i*100)/100.00
+    }
 
 
     /*ЗАДАНИЕ IV*/
@@ -71,6 +89,11 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+      ListMap(ballsArray.toSeq.sortWith((x, y) => getWeight(x._2._1, x._2._2) < getWeight(y._2._1, y._2._2)):_*).keys.toSeq
+    }
 
+    def getWeight(radius:Int, density: Double): Double ={
+      4.00/3.00*java.lang.Math.PI*Math.pow(radius, 3)*density
+    }
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,4 +1,5 @@
 import utest._
+import java.math
 
 object Test extends TestSuite{
 
@@ -6,7 +7,65 @@ object Test extends TestSuite{
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
-            assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+            assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33,
+                35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93,
+                96, 98, 99))
+        }
+        'test_divBy3Or5 - {
+            assert(Exercises.divBy3Or5(1, 5) == Seq(3, 5))
+            assert(Exercises.divBy3Or5(5, 10) == Seq(5, 6, 9, 10))
+        }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(0, 1) == 0)
+            assert(Exercises.sumOfDivBy3Or5(1, 10) == 33)
+            assert(Exercises.sumOfDivBy3Or5(14, 16) == 15)
+            assert(Exercises.sumOfDivBy3Or5(-15, 15) == 0)
+            assert(Exercises.sumOfDivBy3Or5(1, 1000000) == 1405932684)
+        }
+        'passage - {
+            assert(Exercises.passage(80) == Seq(2,2,2,2,5))
+        }
+        'primeFactor - {
+            assert(Exercises.primeFactor(5) == Seq(5))
+            assert(Exercises.primeFactor(10) == Seq(2,5))
+            assert(Exercises.primeFactor(80) == Seq(2,5))
+            assert(Exercises.primeFactor(1405932) == Seq(2, 3, 11, 10651))
+        }
+        'round2 - {
+            assert(Exercises.round2(3.9999) == 4.00)
+        }
+        'abs - {
+            assert(Exercises.round2(Exercises.abs(Exercises.Vector2D(2, 2*Math.sqrt(3)))) == 4.00)
+        }
+        'scalar - {
+            assert(Exercises.round2(Exercises.scalar(Exercises.Vector2D(2,0),
+                Exercises.Vector2D(2,2*Math.sqrt(3)))) == 4.00)
+        }
+        'cosBetween - {
+            assert(Exercises.round2(Exercises.cosBetween(Exercises.Vector2D(2,0),
+                Exercises.Vector2D(2,2*Math.sqrt(3)))) == 0.5)
+        }
+        'sumScalars - {
+            assert(Exercises.sumScalars(Exercises.Vector2D(2,0), Exercises.Vector2D(2,2*Math.sqrt(3)),
+                Exercises.Vector2D(2,0), Exercises.Vector2D(2,2*Math.sqrt(3))) == 8.00)
+        }
+        'sumCosines - {
+            assert(Exercises.round2(Exercises.sumCosines(Exercises.Vector2D(2,0), Exercises.Vector2D(2,2*Math.sqrt(3)),
+                Exercises.Vector2D(2,0), Exercises.Vector2D(2,2*Math.sqrt(3)))) == 1.00)
+        }
+        'getWeigth - {
+            assert(Exercises.round2(Exercises.getWeight(2, 10.50)) == 351.86)
+        }
+        'sortByHeavyweight - {
+            val res = Exercises.sortByHeavyweight(Exercises.balls)
+            assert(res.head == "Tin")
+            //Проверка последовательности
+            for (i <- 0 to res.length - 2){
+                assert(Exercises.getWeight(Exercises.balls.get(res.apply(i)).toSeq.head._1,
+                    Exercises.balls.get(res.apply(i)).toSeq.head._2) <
+                  Exercises.getWeight(Exercises.balls.get(res.apply(i+1)).toSeq.head._1,
+                      Exercises.balls.get(res.apply(i+1)).toSeq.head._2))
+            }
         }
     }
 }
