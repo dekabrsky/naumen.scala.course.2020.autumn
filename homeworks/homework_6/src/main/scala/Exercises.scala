6c7c2b34398fa4bdc142e59ce7a8289b307d4186
Denis Matafonov <dmatafonov2000@mail.ru>
2020-12-14 21:07:21 +0500

    homework_6 - Матафонов Денис

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,7 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = for (i <- seq.length until 0 by -1) yield seq(i-1)
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +9,10 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = (math.pow((math.sqrt(5) + 1) / 2, idx) / math.sqrt(5) + 0.5).toInt
+  //вывод https://en.wikipedia.org/wiki/Fibonacci_number#Closed-form_expression
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = for (i <- 0 to idx) yield fibonacci4Index(i)
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +20,29 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  /* Если неизмененные символы - все же тоже коды, то:
+  def morse(text: String): String = text
+      .toUpperCase()
+      .split("")
+      .map(c => MORSE.getOrElse(c, c))
+      .mkString(" ")
+  */
 
+  def morse(text: String): String = text
+    .toUpperCase()
+    .split("")
+    .map(c =>
+      if(MORSE.contains(c)) " " + MORSE(c)
+      else if(c == " ") "  "
+      else c)
+    .mkString("")
+    .substring(1)
 
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = text
+    .split("(?=[^a-zA-Zа-яёА-ЯЁ])|(?<=[ ])")
+    .map(word =>
+      if (word.head.isUpper) {
+        word.toLowerCase.reverse.capitalize
+      } else word.reverse)
+    .mkString("")
 }
